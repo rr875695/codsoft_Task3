c57be6732291501f03c7b638dc329c42436d7926
Ravi Ranjan <rr875695@gmail.com>
2023-11-21 09:31:11 +0530

    third commit

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll('a[href^="#"]');
+
+  links.forEach(link => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop - 50, // Adjust the offset as needed
+          behavior: "smooth"
+        });
+      }
+    });
+  });
+});
+
+const nav = document.querySelector(".nav");
+window.addEventListener("scroll", fixNav);
+
+function fixNav() {
+  if (window.scrollY > nav.offsetHeight + 150) {
+    nav.classList.add("active");
+  } else {
+    nav.classList.remove("active");
+  }
+}
